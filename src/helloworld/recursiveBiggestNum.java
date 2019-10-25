85416791f4fb3cafc26b4428ad9ec48444fece86
redelization <ricardosmithco@gmail.com>
2019-10-25 15:59:58 -0500

    added comments for readability

--- src/helloworld/recursiveBiggestNum.java
+++ src/helloworld/recursiveBiggestNum.java
@@ -1,10 +1,9 @@
 package helloworld;
 
 public class recursiveBiggestNum {
-	static int biggest = Integer.MIN_VALUE;
+	static int biggest = Integer.MIN_VALUE;		// static variables to be used in this entire class.
 	static int z = 0;;
 	
-	// i have not completed this !!!!
 	
 	public static void main(String [] args) {
 		
@@ -17,8 +16,8 @@ public class recursiveBiggestNum {
 		
 	}
 	
-	 public static int findMax( int arr[]) {
-		 int biggest = arr[0];
+	 public static int findMax( int arr[]) { // ITERATIVE: this method checks for the largest element in the array
+		 int biggest = arr[0];				 // starting with the first element arr[0].
 		 
 		 for(int i =0; i < arr.length; i++) {
 			 if(arr[i] > biggest) {
@@ -29,19 +28,19 @@ public class recursiveBiggestNum {
 	 }	
 	
  
-	 public static int recursiveMax(int arr[], int x) {
-		 
-		 if(x < 0) {
-			 return biggest;
+	 public static int recursiveMax(int arr[], int x) { // RECURSIVE: this method checks for the largest number in the array,
+		 												// from the end of the array, to the beginning.
+		 if(x < 0) {									// this method takes in an array, and x = arr.length-1, which is the last
+			 return biggest;							// index of the array -- starting point for this algorithm
 		 }if(arr[x] > biggest) {
 			 biggest = arr[x];
 		 }
 		 return recursiveMax(arr, (x-1));
 	 }
 	 
-	 public static int reversedRecursiveMax(int arr[], int z) { 
-		 
-		 if(z == arr.length) {
+	 public static int reversedRecursiveMax(int arr[], int z) { // RECURSIVE: this method checks for the largest number in the array,
+		 														// from the beginning of the array: arr[0] until the end.		 
+		 if(z == arr.length) {									// this method takes in an array and z=0, as parameters.
 			 return biggest;
 		 }else if(arr[z] > biggest) {
 			 biggest = arr[z];
